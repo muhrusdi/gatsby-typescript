a6a478acf4c182ee479a7fe7165e6c731bdfbc97
Muhammad Rusdi <muhammadrusdiid@gmail.com>
2020-02-11 00:26:24 +0800

    resolve path alias

---
+++ config.js
@@ -0,0 +1,42 @@
+module.exports.pathAlias = [
+  {
+    alias: "components",
+    path: "components"
+  },
+  {
+    alias: "containers",
+    path: "containers"
+  },
+  {
+    alias: "templates",
+    path: "templates"
+  },
+  {
+    alias: "images",
+    path: "images"
+  },
+  {
+    alias: "utils",
+    path: "utils"
+  },
+  {
+    alias: "layout",
+    path: "containers/layouts"
+  },
+  {
+    alias: "hocs",
+    path: "hocs"
+  },
+  {
+    alias: "hooks",
+    path: "hooks"
+  },
+  {
+    alias: "contexts",
+    path: "contexts"
+  },
+  {
+    alias: "config",
+    path: "config"
+  }
+];
