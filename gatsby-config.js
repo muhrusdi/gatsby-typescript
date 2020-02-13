e4456bab50e2027f9e47003842f6b1cf03792565
Muhammad Rusdi <muhammadrusdiid@gmail.com>
2020-02-13 16:18:22 +0700

    change readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: "Gatsby Default Starter",
-    description: "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
-    author: "@muhrusdiid",
+    description:
+      "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
+    author: "@muhrusdiid"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
@@ -10,8 +11,8 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     "gatsby-transformer-sharp",
     "gatsby-plugin-styled-components",
@@ -21,31 +22,31 @@ module.exports = {
       resolve: "gatsby-plugin-import",
       options: {
         libraryName: "antd",
-        style: true,
-      },
+        style: true
+      }
     },
     {
       resolve: "gatsby-plugin-less",
       options: {
         modifyVars: {},
-        javascriptEnabled: true,
-      },
+        javascriptEnabled: true
+      }
     },
     "gatsby-plugin-typescript",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
+        name: "gatsby-starter",
         short_name: "starter",
         start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
+        background_color: "#fff",
+        theme_color: "#333",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
-      },
-    },
+        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 };
