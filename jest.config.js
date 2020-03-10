cc2a3d7ebba50c2b54e4d0ffe66549c37bf6a60d
Muhammad Rusdi <muhammadrusdi@icloud.com>
2020-03-10 23:37:32 +0800

    fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,24 @@
+const path = require("path");
+
 module.exports = {
+  roots: [path.resolve(__dirname, "./src")],
+  setupFilesAfterEnv: [path.resolve(__dirname, "./setup-test-env.js")],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js",
+    "^.+\\.(tsx?|jsx?)$": "<rootDir>/jest-preprocess.js",
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx)$",
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/file-mock.js",
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["node_modules", ".cache"],
+  testPathIgnorePatterns: ["node_modules", ".cache", "public"],
   transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
   globals: {
     __PATH_PREFIX__: "",
   },
+  collectCoverage: false,
   testURL: "http://localhost",
+  testEnvironment: "jsdom",
 };
